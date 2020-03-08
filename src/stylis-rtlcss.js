e78c86fb8687d9d4599c6be1feaa8f7cc59ffa8b
Maged Mohamed <MagedMohammed16@GMail.Com>
2020-03-09 01:14:29 +0200

    configuration for rtlcss

---
+++ src/stylis-rtlcss.js
@@ -0,0 +1,30 @@
+// @flow
+
+import rtlcss from "rtlcss";
+
+// https://github.com/thysultan/stylis.js#plugins
+const STYLIS_CONTEXTS = {
+  POST_PROCESS: -2,
+  PREPARATION: -1,
+  NEWLINE: 0,
+  PROPERTY: 1,
+  SELECTOR_BLOCK: 2,
+  AT_RULE: 3
+};
+
+export type StylisContextType = $Values<typeof STYLIS_CONTEXTS>;
+
+export const STYLIS_PROPERTY_CONTEXT = STYLIS_CONTEXTS.PREPARATION;
+
+function stylisRTLCSS(context: StylisContextType, content: string): ?string {
+  if (context === STYLIS_PROPERTY_CONTEXT) {
+    return rtlcss.process(content);
+  }
+}
+
+// stable identifier that will not be dropped by minification unless the whole module
+// is unused
+/*#__PURE__*/
+Object.defineProperty(stylisRTLCSS, "name", { value: "stylisRTLCSS" });
+
+export default stylisRTLCSS;
