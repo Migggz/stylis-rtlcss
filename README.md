4ba3191455d9ad2c4293ed04ba32ba29e27f82f3
Maged Mohamed <MagedMohammed16@GMail.Com>
2020-03-09 02:02:26 +0200

    added control directives

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # stylis-rtlcss
+
+Stylis RTL plugin based on [RTLCSS](https://rtlcss.com/)
+
+## Install
+
+###### Yarn
+
+```cmd
+yarn add stylis-rtlcss
+```
+
+###### NPM
+
+```cmd
+npm i stylis-rtlcss
+```
+
+---
+
+## Usage with Styled-Components v5+
+
+```javascript
+import styled, { StyleSheetManager } from "styled-components";
+import rtlPlugin from "stylis-plugin-rtl";
+
+const StyledDiv = styled.div`
+  margin-left: 10px;
+`;
+
+class App extends Component {
+  return (
+    <StyleSheetManager stylisPlugins={[rtlPlugin]}>
+      <StyledDiv>Margin will be on RIGHT!</StyledDiv>
+    </StyleSheetManager>
+  )
+}
+```
+
+---
+
+<!--
+## Options
+#### - Supports Control Directives -->
--- src/stylis-rtlcss.js
+++ src/stylis-rtlcss.js
@@ -1,5 +1,4 @@
 // @flow
-
 import rtlcss from "rtlcss";
 
 // https://github.com/thysultan/stylis.js#plugins
@@ -15,10 +14,94 @@ const STYLIS_CONTEXTS = {
 export type StylisContextType = $Values<typeof STYLIS_CONTEXTS>;
 
 export const STYLIS_PROPERTY_CONTEXT = STYLIS_CONTEXTS.PREPARATION;
+const options = {
+  autoRename: false,
+  autoRenameStrict: false,
+  blacklist: {},
+  clean: true,
+  greedy: false,
+  processUrls: false,
+  stringMap: [
+    {
+      name: "left-right",
+      priority: 100,
+      search: ["left", "Left", "LEFT"],
+      replace: ["right", "Right", "RIGHT"],
+      options: {
+        scope: "*",
+        ignoreCase: false
+      }
+    },
+    {
+      name: "ltr-rtl",
+      priority: 100,
+      search: ["ltr", "Ltr", "LTR"],
+      replace: ["rtl", "Rtl", "RTL"],
+      options: {
+        scope: "*",
+        ignoreCase: false
+      }
+    }
+  ],
+  useCalc: false
+};
+const plugins = [
+  {
+    name: "ignore plugin",
+    priority: 100,
+    directives: {
+      control: {
+        ignore: {
+          // the expected node types
+          expect: { atrule: true, comment: true, decl: true, rule: true },
+          // local variable to store ending node
+          endNode: null,
+          // prevent processing all nodes except comments indicating the end of this directive
+          begin: function(node, metadata, context) {
+            // find the ending node in case of self closing directive
+            if (!this.endNode && metadata.begin && metadata.end) {
+              var n = node;
+              while (n && n.nodes) {
+                n = n.nodes[n.nodes.length - 1];
+              }
+              this.endNode = n;
+            }
+            var prevent = true;
+            if (
+              node.type === "comment" &&
+              (node.text === "!rtl:end:ignore" ||
+                node.text === "rtl:end:ignore")
+            ) {
+              prevent = false;
+            }
+            return prevent;
+          },
+          // deactivate the directive if:
+          //  1. block directive and the node is comment
+          //  2. self closing directive and node is endNode
+          end: function(node, metadata, context) {
+            if (
+              (metadata.begin !== metadata.end && node.type === "comment") ||
+              (metadata.begin && metadata.end && node === this.endNode)
+            ) {
+              // clear ending node
+              this.endNode = null;
+              // deactivate
+              return true;
+            }
+            // keep ignoring
+            return false;
+          }
+        }
+      },
+      value: []
+    }
+  }
+];
 
 function stylisRTLCSS(context: StylisContextType, content: string): ?string {
   if (context === STYLIS_PROPERTY_CONTEXT) {
-    return rtlcss.process(content);
+    return rtlcss.process(content, [, options, plugins]);
   }
 }
 
